a97ad0e5f10c6d24cd6049c0d3c72fdf3385926e
Pat Bills <billspat@msu.edu>
2024-12-04 23:26:34 -0500

    convert hour num to human reaable time

--- lib/converters.py
+++ lib/converters.py
@@ -6,6 +6,8 @@ None of these are rounded  - setting the precision is the job
 of the calling program 
 """
 
+from datetime import time
+
 def c2f(c:float)->float:
     f = c*(9/5) + 32
     return(f)
@@ -56,3 +58,31 @@ def degree2compass(deg:float)->str:
     
     return(direction)
     
+    
+def hour_number2clock_str(h:int)->str:
+    """convert the hour number ( 1 to 24) which is output by hourly
+    weather summary into a human readable time interval
+    
+    hour 1 => '12:00AM-12:59AM'
+
+    Args:
+        h (int): hour number 1 is first hour of the day
+
+    Returns:
+        str: time interval in 12hr clock
+    """
+    
+    # try:
+    #     h = int(h)
+    # except Exception as e:
+    #     return ""
+    
+    #h = int(h)
+    if (h > 24) or (h < 1):
+        return("")
+    
+    start_time = time((h-1),0)
+    end_time = time(start_time.hour, 59)
+    time_interval_str = start_time.strftime("%-I:%M") + "-" + end_time.strftime("%-I:%M %p")
+    
+    return(time_interval_str)    
